a62a11032895d71e9083352cc474ebed103ac800
frecco <fredrik.holmen@certitrade.se>
2019-09-17 13:46:41 +0200

    Added not added changes

--- Cancel/index.ts
+++ Cancel/index.ts
@@ -1,21 +1,19 @@
 import * as isoly from "isoly"
 import * as authly from "authly"
-import { Creatable as CancelCreatable } from "./Creatable"
 
 export interface Cancel {
 	id: authly.Identifier,
 	descriptor?: string,
 	reference: string,
-	created?: isoly.DateTime,
+	created: isoly.DateTime,
 }
 
 export namespace Cancel {
 	export function is(value: Cancel | any): value is Cancel {
-		return true
-	}
-	export type Creatable = CancelCreatable
-	export namespace Creatable {
-		// tslint:disable-next-line: no-shadowed-variable
-		export const is = CancelCreatable.is
+		return typeof value == "object" &&
+		       authly.Identifier.is(value.id) &&
+		       (value.descriptor == undefined || typeof value.descriptor == "string") &&
+		       typeof value.reference == "string" &&
+		       isoly.DateTime.is(value.created)
 	}
 }
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import * as Acquirer from "./Acquirer"
 import { Authorization } from "./Authorization"
+import { Capture } from "./Capture"
 import { Cancel } from "./Cancel"
 import { Card } from "./Card"
 import { Merchant } from "./Merchant"
@@ -8,6 +9,7 @@ import { Redirect } from "./Redirect"
 export {
 	Acquirer,
 	Authorization,
+	Capture,
 	Cancel,
 	Card,
 	Merchant,
