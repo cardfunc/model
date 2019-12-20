0db822c025a78be6d020e9bef32857ea817fadd2
Simon Mika <simon@mika.se>
2019-12-20 17:15:41 +0100

    Added merchant url and mid.

--- Merchant/Key.spec.ts
+++ Merchant/Key.spec.ts
@@ -7,6 +7,7 @@ describe("Merchant.Key", () => {
 		aud: "private",
 		sub: "par9",
 		name: "test",
+		url: "http://example.com",
 		descriptor: "test transaction",
 		country: "SE",
 		acquirer: {
@@ -15,6 +16,7 @@ describe("Merchant.Key", () => {
 			key: "36e74a69-7fee-4a37-bcd9-6a1422028ff3",
 		},
 		mcc: "1234",
+		mid: "1234",
 		bin: {
 			mastercard: "134678",
 			visa: "1234",
--- Merchant/index.spec.ts
+++ Merchant/index.spec.ts
@@ -4,6 +4,7 @@ describe("Merchant", () => {
 	const merchant = {
 		id: "par9",
 		name: "test",
+		url: "http://example.com",
 		descriptor: "test transaction",
 		country: "SE",
 		acquirer: {
@@ -12,12 +13,13 @@ describe("Merchant", () => {
 			key: "36e74a69-7fee-4a37-bcd9-6a1422028ff3",
 		},
 		mcc: "1234",
+		mid: "1234",
 		bin: {
 			mastercard: "134678",
 			visa: "1234",
 		},
 	}
-	it("is", () => expect(model.Merchant.is({ id: "123", name: "Merchant Ltd.", country: "GB", acquirer: { protocol: "clearhaus", url: "https://example.com/", key: "secret-api-key" }, mcc: "1234", bin: { visa: "1234", mastercard: "54321" } })).toBeTruthy())
+	it("is", () => expect(model.Merchant.is({ id: "123", name: "Merchant Ltd.", url: "http://example.com", country: "GB", acquirer: { protocol: "clearhaus", url: "https://example.com/", key: "secret-api-key" }, mcc: "1234", mid: "1234", bin: { visa: "1234", mastercard: "54321" } })).toBeTruthy())
 	it("is 2", () => {
 		expect(model.Merchant.is(merchant)).toBeTruthy()
 	})
