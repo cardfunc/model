24274de3d20b8eb565e85d558ead77801b980db3
Simon Mika <simonmika@users.noreply.github.com>
2020-02-12 00:29:54 +0100

    Made mid, mcc and bin optional. (#44)

--- Merchant/Key.ts
+++ Merchant/Key.ts
@@ -13,8 +13,8 @@ export namespace Key {
 	export function is(value: Key | any): value is Key {
 		return Creatable.is(value) &&
 			authly.Identifier.is((value as any).sub) &&
-			typeof((value as any).iss) == "string" &&
-			typeof((value as any).iat) == "number" &&
+			typeof(value as any).iss == "string" &&
+			typeof(value as any).iat == "number" &&
 			((value as any).aud == "public" || (value as any).aud == "private")
 	}
 	export function flaw(value: any | Key): gracely.Flaw {
@@ -22,10 +22,10 @@ export namespace Key {
 			type: "model.Merchant.Key",
 			flaws: typeof(value) != "object" ? undefined :
 				[
-					typeof(value.sub) == "string" || { property: "sub", type: "authly.Identifier", condition: "Merchant identifier." },
-					typeof(value.iss) == "string" || { property: "iss", type: "string", condition: "Key issuer." },
-					typeof(value.aud) == "string" || { property: "aud", type: `"public" | "private"`, condition: "Key audience." },
-					typeof(value.iat) == "number" || { property: "iat", type: "number", condition: "Issued timestamp." },
+					typeof value.sub == "string" || { property: "sub", type: "authly.Identifier", condition: "Merchant identifier." },
+					typeof value.iss == "string" || { property: "iss", type: "string", condition: "Key issuer." },
+					typeof value.aud == "string" || { property: "aud", type: `"public" | "private"`, condition: "Key audience." },
+					typeof value.iat == "number" || { property: "iat", type: "number", condition: "Issued timestamp." },
 					...Creatable.flaw(value).flaws || [],
 				].filter(gracely.Flaw.is) as gracely.Flaw[],
 		}
