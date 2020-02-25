95ca0e932ad95c8a229258f5c8f13b93aa94481e
Simon Mika <simonmika@users.noreply.github.com>
2020-02-25 15:06:03 +0100

    Added verification of production account. (#47)

---
+++ Account.spec.ts
@@ -0,0 +1,13 @@
+import * as model from "./index"
+
+describe("Account", () => {
+	it("verify production", async () =>
+		expect(await model.Account.verify("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJDYXJkRnVuYyIsImlhdCI6MTU4MTU5ODU4MTM1NCwiYXVkIjoicHJvZHVjdGlvbiIsImlkIjoiUVlrbEdYX0sifQ.iyY_NTgzqHt1zJKmeEpm6yylWw1uLYLbW55KFMv7Q1z0hq90E9VfXhAgXbNQTQrj1ZNh1Xd_yQ6PMXQtT6CFMSAOsVCzvszk7r0_Big8yZSe8GjOIhpWdoyy55yPHDz5bh-9QffdDW_S3TGJGqwC9QBWRQXqJElKWJEY3nK1rfGFD-Ip7BY5MM3a4YpBeV6n2EIGtF4Rg5eM24WXuc9Kk1Buh-S2hwoXFV_pWu6zPWfSmWuIIOPiFE3w4FpzTu2yDvcE37R20yCYDD7P1tiA5rnNfy9CFFo5V-QlI_t3ybBOFf2_vRoKoT6Flm19fGJcRdWbulWXZQn9wQg2e654XA"))
+		.toEqual({
+			aud: "production",
+			iat: 1581598581354,
+			id: "QYklGX_K",
+			iss: "CardFunc",
+		})
+	)
+})
