d350a74711e44096dd5fa0cee3115ca3f4a442f8
robinnedergard <60611967+robinnedergard@users.noreply.github.com>
2020-05-22 13:43:41 +0200

    risk-info for use with 3-D Secure V2 (#59)

---
+++ AuthorizationV2/Risk/GiftCard.ts
@@ -0,0 +1,17 @@
+import * as isoly from "isoly"
+
+export interface GiftCard {
+	amount?: string
+	count?: string
+	currency?: isoly.Currency
+}
+
+// tslint:disable-next-line: no-namespace
+export namespace GiftCard {
+	export function is(value: GiftCard | any): value is GiftCard {
+		return typeof value == "object" &&
+			(value.amount == undefined || typeof value.amount == "string" && /^\d{0,15}$/.test(value.amount)) &&
+			(value.count == undefined || typeof value.count == "string" && /^\d{2}$/.test(value.count)) &&
+			(value.currency == undefined || isoly.Currency.is(value.currency))
+	}
+}
