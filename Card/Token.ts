1248ac1ca9979b8b7485782d1bc27530338886c3
Robin Nedergård <robin.nedergard@payfunc.com>
2020-06-15 15:51:58 +0200

    token change

--- Card/Token.ts
+++ Card/Token.ts
@@ -2,15 +2,15 @@ import * as authly from "authly"
 import { verify as verifyToken } from "../verify"
 
 export interface Token {
-	id: authly.Identifier
-	pares?: string
+	type: "authorization" | "recurring" | "network"
+	card: authly.Identifier
 }
 
 export namespace Token {
 	export function is(value: Token | any): value is Token {
 		return typeof value == "object" &&
-			authly.Identifier.is(value.id) &&
-			(value.pares == undefined || typeof value.pares == "string")
+			(value.type == "authorization" || value.type == "recurring" || value.type == "network") &&
+			authly.Identifier.is(value.card)
 	}
 	export async function verify(token: authly.Token): Promise<Token & authly.Payload | undefined> {
 		const result = await verifyToken(token)
