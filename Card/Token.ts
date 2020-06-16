6ce331d52ab3f0ad7ae647b89ce0b532e7dcf9e9
Robin Nedergård <robin.nedergard@payfunc.com>
2020-06-16 16:07:19 +0200

    replaced "_" with " "

--- Card/Token.ts
+++ Card/Token.ts
@@ -2,14 +2,14 @@ import * as authly from "authly"
 import { verify as verifyToken } from "../verify"
 
 export interface Token {
-	type: "single_use" | "recurring"
+	type: "single use" | "recurring"
 	card: authly.Identifier
 }
 
 export namespace Token {
 	export function is(value: Token | any): value is Token {
 		return typeof value == "object" &&
-			(value.type == "single_use" || value.type == "recurring") &&
+			(value.type == "single use" || value.type == "recurring") &&
 			authly.Identifier.is(value.card)
 	}
 	export async function verify(token: authly.Token): Promise<Token & authly.Payload | undefined> {
