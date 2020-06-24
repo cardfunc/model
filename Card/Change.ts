f2935759409aee43842980012cabcb2d666594ac
Robin Nedergård <robin.nedergard@payfunc.com>
2020-06-24 12:43:04 +0200

    added flaw

--- Card/Change.ts
+++ Card/Change.ts
@@ -1,3 +1,4 @@
+import * as gracely from "gracely"
 import { Creatable } from "./Creatable"
 import { Expires } from "./Expires"
 
@@ -10,5 +11,17 @@ export namespace Change {
 			(value.expires == undefined || Expires.is(value.expires)) &&
 			(value.csc == undefined || typeof(value.csc) == "string") &&
 			(value.pares == undefined || typeof(value.pares) == "string")
+	}
+	export function flaw(value: Change | any): gracely.Flaw {
+		return {
+			type: "model.Card.Change",
+			flaws: typeof value != "object" ? undefined :
+				[
+					value.pan == undefined || typeof(value.pan) == "string" || { property: "pan", type: "string | undefined" },
+					value.expires == undefined || Expires.is(value.expires) || { property: "expires", type: "string | undefined" },
+					value.csc == undefined || typeof(value.csc) == "string" || { property: "csc", type: "string | undefined" },
+					value.pares == undefined || typeof(value.pares) == "string" || { property: "pares", type: "string | undefined" },
+				].filter(gracely.Flaw.is) as gracely.Flaw[],
+		}
 	}
 }
