1678aeddb5bce7916aa758f249c13304b2f26913
robinnedergard <60611967+robinnedergard@users.noreply.github.com>
2020-12-17 20:03:27 +0100

    cleanup (#93)

--- index.ts
+++ index.ts
@@ -1,31 +1,12 @@
 import { Account } from "./Account"
 import { Acquirer } from "./Acquirer"
-import { Address } from "./Address"
 import { Authorization } from "./Authorization"
 import { Browser } from "./Browser"
 import { Capture } from "./Capture"
 import { Cancel } from "./Cancel"
 import { Card } from "./Card"
-import { Customer } from "./Customer"
-import { Email } from "./Email"
 import { Merchant } from "./Merchant"
-import { PhoneNumber } from "./PhoneNumber"
 import { Redirect } from "./Redirect"
 import { Refund } from "./Refund"
 
-export {
-	Account,
-	Acquirer,
-	Address,
-	Authorization,
-	Browser,
-	Capture,
-	Cancel,
-	Card,
-	Customer,
-	Email,
-	Merchant,
-	PhoneNumber,
-	Redirect,
-	Refund,
-}
+export { Account, Acquirer, Authorization, Browser, Capture, Cancel, Card, Merchant, Redirect, Refund }
