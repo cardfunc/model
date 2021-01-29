c68d834986bbc50ed71727f5036fb7a36dad6ec7
Emma Nilsson <31855183+nilssonemma@users.noreply.github.com>
2021-01-29 09:09:52 +0100

    Added intergiro  acquirer protocol. (#112)

--- Acquirer/Protocol.ts
+++ Acquirer/Protocol.ts
@@ -1,7 +1,7 @@
-export type Protocol = "clearhaus"
+export type Protocol = "clearhaus" | "intergiro"
 
 export namespace Protocol {
 	export function is(value: Protocol | any): value is Protocol {
-		return typeof value == "string" && value == "clearhaus"
+		return typeof value == "string" && (value == "clearhaus" || value == "intergiro")
 	}
 }
