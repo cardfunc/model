c410db9228d0e553f53063e7fb489676af9fcd25
florianebmeier <70569666+florianebmeier@users.noreply.github.com>
2021-05-30 22:57:26 +0200

    Apply pan typeguard (#126)
    * Apply Pan typeguard in Card Creatable
    
    * Added test

--- Card/Pan.spec.ts
+++ Card/Pan.spec.ts
@@ -1,5 +1,6 @@
 import * as model from "../index"
 
 describe("Card.Pan", () => {
+	it("Typeguard", () => expect(model.Card.Pan.is("5105105105105100")).toEqual(true))
 	it("last4", () => expect(model.Card.Pan.last4("5105105105105100")).toEqual("5100"))
 })
